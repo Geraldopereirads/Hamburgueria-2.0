a842fdac37081b51cab01575b23896ccb0ad5867
geraldopereirads <geraldo.pereirads@hotmail.com>
2024-07-01 23:37:43 -0300

    fix: repair link api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://hamburgueria-kenzie-v2.herokuapp.com/',
+  baseURL: 'https://hamburgueria-kenzie-json-serve.herokuapp.com/',
   timeout: 6000,
 });
